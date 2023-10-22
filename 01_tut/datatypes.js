8ca80936fd298d4988953c3a285a2bec3c5d5ed1
im-yashs <98693400+im-yashs@users.noreply.github.com>
2023-10-22 11:33:46 +0000

    datatypes

---
+++ 01_tut/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all the JS code as newer version
+
+// alert=(3+3) //we are using node JS,not browser
+
+//Datatypes
+/* 
+number=> 2 to power 53
+bigint
+String=" "
+boolean=true/false
+null=>Stand alone value
+Symbol => Unique Datatypes
+
+*/
+
+console.log(typeof undefined) // undefined
+console.log(typeof null);  //Object
