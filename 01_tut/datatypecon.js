4f7a541fe04cd0556c203058c2acfa0f7b4dad42
im-yashs <98693400+im-yashs@users.noreply.github.com>
2023-10-26 06:23:04 +0000

    Datatype coversion

--- 01_tut/datatypecon.js
+++ 01_tut/datatypecon.js
@@ -1,2 +1,26 @@
-let score="20"
-console.log(typeof score);
+let score="20abcnode 01_tut/datatypecon "
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber= Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//*****Operations*********
+
+let str1 = "hello"
+let str2 =" yash"
+let str3 =str1 + str2
+console.log(str3);
+
+console.log(1+"2");
+console.log("1"+2);
+
+// console.log(1+"2"+"2");
+// console.log("1"+"2"+2);
+// console.log(1+2+"2");
+// console.log(1+"2"+2);
+// console.log(1+2+2);
+
+ console.log(node 01_tut/datatypecon
+ true);
